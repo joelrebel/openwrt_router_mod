959331b1a14653dd44f224e7c666f142c5eb9c8f
root <root@nixbox.(none)>
2010-10-30 16:38:38 +0530

    loads of fixes, ssh keys generated, pppd working

--- rootfs/etc/itxscripts/functions.sh
+++ rootfs/etc/itxscripts/functions.sh
@@ -2,14 +2,9 @@
 #ned 
 set -x
 
-POWERCUT_FLAG_COUNT=0;
-POWERCUT_FLAG_MAXCOUNT=11; # $POWERCUT_FLAG_COUNT * LOOP_INTERVAL = number of seconds the script would wait to take action on the router..
-			   # once the script hits maxcount, we run halt	
-
 # function must be called with  log_line [debug|error] "some message here"
 # Timestamp needs to be added for each logline
-function logline 
-{	
+logline() { 
 	if [[ $LOG_LEVEL -eq "0" ]];
 	then
 		if [ "$1" == "error" ];
@@ -20,26 +15,31 @@ function logline
 	
 	if [[ $LOG_LEVEL -eq "1" ]];
 	then
-		if   [ "$1" == "debug" ];
+		if   [ "$1" == "debug" ]; 
 		then
 			echo "debug: $2" 
+		elif [ "$1" == "error" ];
+		then
 			echo "error: $2" 
 		fi
 	fi
 	
 	if [[ $LOG_LEVEL -eq "2" ]];
 	then
-		if [ "$1" == "info" ] ;
+		if [ "$1" == "info" ];
 		then	
-			echo "info: $2" 
+			echo "info: $2"
+		elif [ "$1" == "error" ];
+		then
 			echo "error: $2" 
+		elif [ "$1" == "debug" ];
+		then
 			echo "debug: $2" 
 		fi
 	fi	
 }
 
-function powercut_flag_check 
-{
+powercut_flag_check() {
 	if [[ ! -f .powercut_flag ]];
 	then
 		touch .powercut_flag
@@ -48,7 +48,7 @@ function powercut_flag_check
 	then	
 		if [[ $POWERCUT_FLAG_COUNT -le $POWERCUT_FLAG_MAXCOUNT ]];		
 		then
-			$POWERCUT_FLAG_COUNT=$(( $POWERCUT_FLAG_COUNT + 1 ))
+			POWERCUT_FLAG_COUNT=$(( $POWERCUT_FLAG_COUNT + 1 ))
 			return $POWERCUT_FLAG_COUNT #max count not yet reached
 	
 		elif [[ $POWERCUT_FLAG_COUNT -eq $(( $POWERCUT_FLAG_MAXCOUNT+1 )) ]]; #12 * LOOP_INTERVAL = 60 secs/ 1min 
@@ -60,38 +60,69 @@ function powercut_flag_check
 	fi
 }
 
-#ssh keys would be needed for server and router
-function turn_atom_normal
-{
-	echo "->turn_atom_normal<-"
+checkrun_atom_ppp() {
+	PPP_PID=$(ssh -i id_rsa root@${ATOM_IP} -p2222 "pidof pppd") 
+	if [ ! $PPP_PID ];
+	then
+		ssh -i id_rsa root@${ATOM_IP} -p2222 "nohup /usr/sbin/pppoe-start"
+	else 
+		echo "pppd active at $PPP_PID on ${ATOM_IP}"
+	fi	
+}
+turn_atom_off() {
+	ssh -i id_rsa root@${ATOM_IP} -p2222 "/sbin/halt"
 }
 
-function turn_atom_gateway
-{	
-	echo "->turn_atom_gateway<-"
+turn_atom_normal() {
+	ssh -i id_rsa root@${ATOM_IP} -p2222 "/usr/sbin/pppoe-stop; ifconfig $ATOM_GATEWAY_INTF down"
+	if [ "$1" == "poweroff" ];
+	then
+		turn_atom_off
+	fi	
 }
 
-function turn_self_gateway 
-{
+turn_atom_gateway() {
+	if [ "$MY_STATUS" == "GATEWAY" ];
+	then
+		echo "WTF MY_STATUS -> $MY_STATUS, im not letting a splitbrain!" 
+	else 
+		ssh -i id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF $GATEWAY_IP"
+		checkrun_atom_ppp
+	fi	
+}
+
+turn_self_gateway() { 
 	echo "->turn_self_gateway<-"
+	ifconfig $MY_GATEWAY_INTF $GATEWAY_IP up
+	>/etc/itxscripts/pppd.log
+	/usr/sbin/pppd plugin rp-pppoe.so br-lan noipdefault noauth default-asyncmap defaultroute hide-password nodetach mtu 1492 mru 1492 noaccomp nodeflate nopcomp novj novjccomp user slrebello password sallu199 lcp-echo-interval 20 lcp-echo-failure 3 maxfail 10 logfile /etc/itxscripts/pppd.log debug &
+	echo -e 'nameserver 208.67.220.220\nnameserver 8.8.8.8' >/etc/resolv.conf	
+
 }
 
-function turn_self_normal
-{
+turn_self_normal() {
+	
 	echo "->turn_self_normal<-"
+	PPP_PID=$(pidof pppd)
+	if [ $PPP_PID ];
+	then
+		kill -s SIGTERM $PPP_PID
+	fi
+
+	ifconfig  $MY_GATEWAY_INTF down
 }
 
 
-function check_mystatus
-{
-	MY_CURRENT_IP=$(ifconfig $MY_GATEWAY_INTERFACE | awk -F ':' '/inet addr/{print $2}' | sed -e 's/Bcast//g')
+check_mystatus() {
+	
+	MY_CURRENT_IP=$(ifconfig $MY_GATEWAY_INTF | awk -F ':' '/inet addr/{print $2}' | sed -e 's/  Bcast//g')
 	logline info  "MY_CURRENT_IP -> $MY_CURRENT_IP, MY_IP -> $MY_IP" 
-	if [ "$MY_CURRENT_IP" ==  $MY_IP ];
+	if [ "$MY_CURRENT_IP" ==  "$MY_IP" ];
 	then
 		MY_STATUS=NORMAL
 		return 3
 
-	elif [ "$MY_CURRENT_IP" == $GATEWAY_IP ];
+	elif [ "$MY_CURRENT_IP" ==  "$GATEWAY_IP" ];
 	then
 		MY_STATUS=GATEWAY
 		return 2
@@ -99,18 +130,26 @@ function check_mystatus
 }
 
 
-function poll_gpio
-{
+poll_gpio() {
 
- #POWER_STATUS=$(cat /proc/diag/button/ses)
- POWER_STATUS=$(cat /tmp/diag/button/ses)
+ POWER_STATUS=$(cat /proc/diag/button/ses)
  if [[ $POWER_STATUS -eq 0 ]];
  then
+	DIAG_LED=$(cat /proc/diag/led/diag)
+ 	if [[ $DIAG_LED -eq 1 ]];
+	then
+		echo 0 >/proc/diag/led/diag
+	fi
  	POWER_STATUS=ON
 	return 0
 
  elif [[ $POWER_STATUS -eq 1 ]];
- then
+ then	
+ 	DIAG_LED=$(cat /proc/diag/led/diag)
+ 	if [[ $DIAG_LED -eq 0 ]];
+	then
+		echo 1 >/proc/diag/led/diag
+	fi	
  	POWER_STATUS=OFF
 	return 1
  fi	
@@ -118,15 +157,15 @@ function poll_gpio
   	 
 }
 
-function ping_check
-{
+ping_check() {
 	#pull out mac addr, compare and set MAC to ATOM|ME|UNKNOWN	
+	PING_COUNT=0
 	TO_PING=$1
 	COUNT=0
 
 	while [[ $COUNT -ne 5 ]]
 	do	
-		arping -c1 $TO_PING -w2 -I $INTF
+		arping -c1 $TO_PING -w2 -I $MY_GATEWAY_INTF
 		# returns 1 if sucess,
 		# returns 0 if fail
 		
@@ -136,13 +175,14 @@ function ping_check
 		fi
 
 		COUNT=$(( $COUNT + 1 ))	
-		sleep 1
 	done	
 
 	if [[ $PING_COUNT -ge 3 ]];
-	then
+	then	
+		STATUS=UP
 		return 0 #$1 reachable
 	else
+		STATUS=DOWN	
 		return 1 #$1 unreachable
 	fi	
 }
--- rootfs/etc/itxscripts/gateway_controller.sh
+++ rootfs/etc/itxscripts/gateway_controller.sh
@@ -23,7 +23,7 @@ set -x
 #
 #router will have secondary ip with  br-lan:1 
 #log levels are
-#LOG_LEVEL='2' 
+LOG_LEVEL='2' 
 # 0 - error logs only
 # 1 - error + debug logs
 # 2 - error + debug  + info logs
@@ -33,208 +33,58 @@ ATOM_MAC="00:1c:c0:d5:ed:60"
 
 MY_IP='192.168.0.4'
 MY_SECONDARY_IP='192.168.0.140'
-MY_GATEWAY_INTF='br-lan' #brwlan
+MY_GATEWAY_INTF='br-lan:1' #brwlan
 
 ATOM_IP='192.168.0.110'
-ATOM_GATEWAY_INTF='eth0'
+ATOM_GATEWAY_INTF='eth0:1'
 
 GATEWAY_IP='192.168.0.1'
 
+POWERCUT_FLAG_COUNT=0;
+POWERCUT_FLAG_MAXCOUNT=3; # $POWERCUT_FLAG_COUNT * LOOP_INTERVAL = number of seconds the script would wait to take action on the router..
+			   # once the script hits maxcount, we run halt	
 
 MY_STATUS='UNKOWN'
 ATOM_STATUS='UNKOWN'
 POWER_STATUS='UNKOWN'
 GATEWAY_STATUS='UNKOWN'
-
-#cat /proc/diag/button/ses  - < AOSS button 
-# 0 = MAINS_ON
-# 1 = MAINS_OFF
-
-POWERCUT_FLAG_COUNT=0;
-POWERCUT_FLAG_MAXCOUNT=11; 
-# $POWERCUT_FLAG_COUNT * LOOP_INTERVAL = number of seconds the script would wait to take action on the router..
-# once the script hits maxcount, we run halt	
-
-# function must be called with  log_line [debug|error] "some message here"
-# Timestamp needs to be added for each logline
-function logline 
-{	
-	if [[ $LOG_LEVEL -eq "0" ]];
-	then
-		if [ "$1" == "error" ];
-		then
-			echo "error: $2" 
-		fi
-	fi
-	
-	if [[ $LOG_LEVEL -eq "1" ]];
-	then
-		if   [ "$1" == "debug" ];
-		then
-			echo "debug: $2" 
-			echo "error: $2" 
-		fi
-	fi
-	
-	if [[ $LOG_LEVEL -eq "2" ]];
-	then
-		if [ "$1" == "info" ] ;
-		then	
-			echo "info: $2" 
-			echo "error: $2" 
-			echo "debug: $2" 
-		fi
-	fi	
-}
-
-function powercut_flag_check 
-{
-	if [[ ! -f .powercut_flag ]];
-	then
-		touch .powercut_flag
-
-	elif [[ -f .powercut_flag ]];
-	then	
-		if [[ $POWERCUT_FLAG_COUNT -le $POWERCUT_FLAG_MAXCOUNT ]];		
-		then
-			$POWERCUT_FLAG_COUNT=$(( $POWERCUT_FLAG_COUNT + 1 ))
-			return $POWERCUT_FLAG_COUNT #max count not yet reached
-	
-		elif [[ $POWERCUT_FLAG_COUNT -eq $(( $POWERCUT_FLAG_MAXCOUNT+1 )) ]]; #12 * LOOP_INTERVAL = 60 secs/ 1min 
-		then
-			return 0 #max count reached
-			
-		fi
-
-	fi
-}
-
-#ssh keys would be needed for server and router
-function turn_atom_normal
-{
-	echo "->turn_atom_normal<-"
-}
-
-function turn_atom_gateway
-{	
-	echo "->turn_atom_gateway<-"
-}
-
-function turn_self_gateway 
-{
-	echo "->turn_self_gateway<-"
-}
-
-function turn_self_normal
-{
-	echo "->turn_self_normal<-"
-}
-
-
-function check_mystatus
-{
-	MY_CURRENT_IP=$(ifconfig $MY_GATEWAY_INTERFACE | awk -F ':' '/inet addr/{print $2}' | sed -e 's/Bcast//g')
-	logline info  "MY_CURRENT_IP -> $MY_CURRENT_IP, MY_IP -> $MY_IP" 
-	if [ "$MY_CURRENT_IP" ==  $MY_IP ];
-	then
-		MY_STATUS=NORMAL
-		return 3
-
-	elif [ "$MY_CURRENT_IP" == $GATEWAY_IP ];
-	then
-		MY_STATUS=GATEWAY
-		return 2
-	fi
-}
-
-
-function poll_gpio
-{
-
- #POWER_STATUS=$(cat /proc/diag/button/ses)
- POWER_STATUS=$(cat /tmp/diag/button/ses)
- if [[ $POWER_STATUS -eq 0 ]];
- then
- 	POWER_STATUS=ON
-	return 0
-
- elif [[ $POWER_STATUS -eq 1 ]];
- then
- 	POWER_STATUS=OFF
-	return 1
- fi	
- 
-  	 
-}
-
-function ping_check
-{
-	#pull out mac addr, compare and set MAC to ATOM|ME|UNKNOWN	
-	TO_PING=$1
-	COUNT=0
-
-	while [[ $COUNT -ne 5 ]]
-	do	
-		arping -c1 $TO_PING -w2 -I $INTF
-		# returns 1 if sucess,
-		# returns 0 if fail
-		
-		if [[ $? -eq 0 ]];
-		then
-	     		PING_COUNT=$(( PING_COUNT + 1 ))
-		fi
-
-		COUNT=$(( $COUNT + 1 ))	
-		sleep 1
-	done	
-
-	if [[ $PING_COUNT -ge 3 ]];
-	then
-		return 0 #$1 reachable
-	else
-		return 1 #$1 unreachable
-	fi	
-}
-
-
+. functions.sh
 
 while [[ 1 ]];
 do	
 	check_mystatus #returns NORMAL|GATEWAY
 	logline debug "MY_STATUS-> $MY_STATUS"
-	exit
 	if [ "$MY_STATUS" == "NORMAL" ];
 	then
 		ping_check $GATEWAY_IP
-		if [[ $? -eq 1 ]];
+		if [ "$STATUS" == "DOWN" ];
 		then
 			GATEWAY_STATUS=DOWN
-		elif [[ $? -eq 0 ]];
+		elif [ "$STATUS" == "UP" ];
 		then
 			GATEWAY_STATUS=UP
-		fi
+		fi	
+
 	elif [ "$MY_STATUS" == "GATEWAY" ];
 	then
 		GATEWAY_STATUS=UP
 	fi
-
+	
 	ping_check $ATOM_IP
-	if [[ $? -eq 1 ]];
+	if [ "$STATUS" == "DOWN" ];
 	then
 		ATOM_STATUS=DOWN
-	elif [[ $? -eq 0 ]];
+	elif [ "$STATUS" == "UP" ];
 	then
 		ATOM_STATUS=UP
 	fi
-
 
 	poll_gpio
 	if [ "$POWER_STATUS" == "ON" ]; #GPIO indicates power is ON
 	then	
 		logline debug "poll_gpio returned POWER_STATUS -> $POWER_STATUS"
 		POWERCUT_FLAG_COUNT=0; # since power is present, resetting flag to 0	
-
-		if [ "$MYSTATUS" == "NORMAL" ]; # Router isnt the gateway and power is present we need to ensure atom is gateway
+		if [ "$MY_STATUS" == "NORMAL" ]; # Router isnt the gateway and power is present we need to ensure atom is gateway
 		then
 			logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS "
 		
@@ -259,7 +109,7 @@ do
 				elif [ "$GATEWAY_STATUS" == "UP" ]; # unkown on gateway ip
 				then
 					logline error "ping_check returned GATEWAY_IP -> REACHABLE"
-					logline error "POWER_STATUS -> $POWER_STATUS, MYSTATUS -> $MYSTATUS, ATOM_STATUS -> $ATOM_STATUS - but GATEWAY_STATUS -> $GATEWAY_STATUS :S"
+					logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MYSTATUS, ATOM_STATUS -> $ATOM_STATUS - but GATEWAY_STATUS -> $GATEWAY_STATUS :S"
 					#do nothing, 
 					#continue in loop
 				fi
@@ -272,13 +122,14 @@ do
 		 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
 				if [ "$GATEWAY_STATUS" == "UP" ]; # 
 		        	then
-                                	logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
+                                	logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
 				 	#wrong to take for granted atom would be the gateway, could do better here - figure mac etc..
 				 	
-					$TEMP=$(ssh $ATOM_IP -p2222 "ifconfig $ATOM_GATEWAY_INTF" | awk -F ':' '/inet addr/{print $2}' | sed -e 's/Bcast//g' )	
+					TEMP=$(ssh -i id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF" | awk -F ':' '/inet addr/{print $2}' | sed -e 's/  Bcast//g' )	
 				 	if [ "$TEMP" == "$GATEWAY_IP" ];
 				 	then
 				 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, ATOM is GATEWAY \0/"
+						checkrun_atom_ppp
 					 else 
 				 		logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, ATOM is NOT the GATEWAY :S"
 						#do nothing,
@@ -288,13 +139,12 @@ do
 				elif [ "$GATEWAY_STATUS" == "DOWN" ];
 			 	then
 			 		
-					logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, Turning atom into gateway .."
+					logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, Turning atom into gateway .."
 					turn_atom_gateway
 				fi
 			
 			fi #end ATOM_STATUS UP
-		
-		elif [ "$MYSTATUS" == "GATEWAY" ]; #Power is present, router is the gateway - we need to check and turn atom into the gateway
+		elif [ "$MY_STATUS" == "GATEWAY" ]; #Power is present, router is the gateway - we need to check and turn atom into the gateway
 		then	
 			logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS"
 			if [ "$ATOM_STATUS" == "DOWN" ]; # ATOM_IP is down, 
@@ -335,7 +185,7 @@ do
 		if [[ $POWERCUT_FLAG_COUNT -eq $POWERCUT_FLAG_MAXCOUNT  ]]; #POWERCUT_FLAG_MAXCOUNT hit, we check atom, gateway - initiate turn_gateway 
 		then
 			
-			if [ "$MYSTATUS" == "NORMAL" ]; # IM not the gateway
+			if [ "$MY_STATUS" == "NORMAL" ]; # IM not the gateway
 			then
 				logline info "POWER_STATUS -> $POWER_STATUS, POWERCUT_FLAG_COUNT -> $POWERCUT_FLAG_COUNT, MY_STATUS -> $MY_STATUS"
 			
@@ -363,6 +213,16 @@ do
 		 			logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
 				
 					turn_atom_normal poweroff
+				    	   
+					ping_check $GATEWAY_IP
+			                if [ "$STATUS" == "DOWN" ];
+                			then
+                        			GATEWAY_STATUS=DOWN
+                			elif [ "$STATUS" == "UP" ];
+                			then
+                        			GATEWAY_STATUS=UP
+                			fi
+					
 					if [ "$GATEWAY_STATUS" == "UP" ]; # 
 		        		then
                                 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS,  GATEWAY_STATUS -> $GATEWAY_STATUS"
@@ -375,7 +235,7 @@ do
 			
 				fi #end ATOM_IP reachable
 
-			elif [ "$MYSTATUS" == "GATEWAY" ]; #Power is out, need to ensure atom is down
+			elif [ "$MY_STATUS" == "GATEWAY" ]; #Power is out, need to ensure atom is down
 			then	
 				logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS"
 				if [ "$ATOM_STATUS" == "DOWN" ]; #  
