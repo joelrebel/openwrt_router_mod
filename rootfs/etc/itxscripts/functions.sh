5cea6cfe0614331f78d41652cc4c36bacf896a17
root <root@nixbox.(none)>
2010-10-31 20:02:17 +0530

    code workinggit statusii, added wol binary, known hosts

--- rootfs/etc/itxscripts/functions.sh
+++ rootfs/etc/itxscripts/functions.sh
@@ -9,7 +9,7 @@ logline() {
 	then
 		if [ "$1" == "error" ];
 		then
-			echo "error: $2" 
+			echo "error: $2" >> $LOG_FILE
 		fi
 	fi
 	
@@ -17,10 +17,10 @@ logline() {
 	then
 		if   [ "$1" == "debug" ]; 
 		then
-			echo "debug: $2" 
+			echo "debug: $2"  >> $LOG_FILE
 		elif [ "$1" == "error" ];
 		then
-			echo "error: $2" 
+			echo "error: $2"  >> $LOG_FILE
 		fi
 	fi
 	
@@ -28,53 +28,69 @@ logline() {
 	then
 		if [ "$1" == "info" ];
 		then	
-			echo "info: $2"
+			echo "info: $2"  >> $LOG_FILE
 		elif [ "$1" == "error" ];
 		then
-			echo "error: $2" 
+			echo "error: $2"  >> $LOG_FILE
 		elif [ "$1" == "debug" ];
 		then
-			echo "debug: $2" 
+			echo "debug: $2" >> $LOG_FILE
 		fi
 	fi	
 }
 
-powercut_flag_check() {
-	if [[ ! -f .powercut_flag ]];
-	then
-		touch .powercut_flag
+powerresume_flag_check() {
+		
+		if [[ $POWERRESUME_FLAG_COUNT -lt $POWERRESUME_FLAG_MAXCOUNT ]];		
+		then
+			POWERRESUME_FLAG_COUNT=$(( $POWERRESUME_FLAG_COUNT + 1 ))
+			return $POWERRESUME_FLAG_COUNT #max count not yet reached
+	
+		elif [[ $POWERRESUME_FLAG_COUNT -eq $POWERRESUME_FLAG_MAXCOUNT ]]; #12 * LOOP_INTERVAL = 60 secs/ 1min 
+		then
+			return 0 #max count reached
+			
+		fi
+}
 
-	elif [[ -f .powercut_flag ]];
-	then	
-		if [[ $POWERCUT_FLAG_COUNT -le $POWERCUT_FLAG_MAXCOUNT ]];		
+powercut_flag_check() {
+		if [[ $POWERCUT_FLAG_COUNT -lt $POWERCUT_FLAG_MAXCOUNT ]];		
 		then
 			POWERCUT_FLAG_COUNT=$(( $POWERCUT_FLAG_COUNT + 1 ))
 			return $POWERCUT_FLAG_COUNT #max count not yet reached
 	
-		elif [[ $POWERCUT_FLAG_COUNT -eq $(( $POWERCUT_FLAG_MAXCOUNT+1 )) ]]; #12 * LOOP_INTERVAL = 60 secs/ 1min 
+		elif [[ $POWERCUT_FLAG_COUNT -eq $POWERCUT_FLAG_MAXCOUNT ]]; #12 * LOOP_INTERVAL = 60 secs/ 1min 
 		then
 			return 0 #max count reached
 			
 		fi
 
-	fi
 }
 
 checkrun_atom_ppp() {
-	PPP_PID=$(ssh -i id_rsa root@${ATOM_IP} -p2222 "pidof pppd") 
 	if [ ! $PPP_PID ];
 	then
-		ssh -i id_rsa root@${ATOM_IP} -p2222 "nohup /usr/sbin/pppoe-start"
+		/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "pidof pppd >/tmp/pppoe.pid"
+		/usr/bin/scp -P2222 -i /etc/itxscripts/id_rsa root@${ATOM_IP}:/tmp/pppoe.pid /tmp/pppoe.pid
+		PPP_PID=$(cat /tmp/pppoe.pid)
+
+		/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "nohup /usr/sbin/pppoe-start"
+
 	else 
 		echo "pppd active at $PPP_PID on ${ATOM_IP}"
 	fi	
 }
 turn_atom_off() {
-	ssh -i id_rsa root@${ATOM_IP} -p2222 "/sbin/halt"
+	PPP_PID=''
+	/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "/sbin/halt"
 }
 
+#confusing name for the fucntion :s
+
 turn_atom_normal() {
-	ssh -i id_rsa root@${ATOM_IP} -p2222 "/usr/sbin/pppoe-stop; ifconfig $ATOM_GATEWAY_INTF down"
+	PPP_PID=''
+	/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "/usr/sbin/pppoe-stop; ifconfig $ATOM_GATEWAY_INTF down"
+	/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222  "/sbin/iptables -F;/sbin/iptables -t nat -F;/sbin/iptables -t mangle -F"
 	if [ "$1" == "poweroff" ];
 	then
 		turn_atom_off
@@ -86,30 +102,39 @@ turn_atom_gateway() {
 	then
 		echo "WTF MY_STATUS -> $MY_STATUS, im not letting a splitbrain!" 
 	else 
-		ssh -i id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF $GATEWAY_IP"
+		/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF $GATEWAY_IP;"
+		/usr/bin/ssh -y -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222  "/bin/bash /opt/server_scripts/iptables"
 		checkrun_atom_ppp
 	fi	
 }
 
+#IPs are swapped between interfaces
+#br-lan turns 192.168.0.1
+#br-lan:1 turns 192.168.0.4
 turn_self_gateway() { 
 	echo "->turn_self_gateway<-"
+
 	ifconfig $MY_GATEWAY_INTF $GATEWAY_IP up
+	ifconfig $MY_SECONDARY_INTF $MY_IP up
+
 	>/etc/itxscripts/pppd.log
 	/usr/sbin/pppd plugin rp-pppoe.so br-lan noipdefault noauth default-asyncmap defaultroute hide-password nodetach mtu 1492 mru 1492 noaccomp nodeflate nopcomp novj novjccomp user slrebello password sallu199 lcp-echo-interval 20 lcp-echo-failure 3 maxfail 10 logfile /etc/itxscripts/pppd.log debug &
 	echo -e 'nameserver 208.67.220.220\nnameserver 8.8.8.8' >/etc/resolv.conf	
+	/usr/sbin/iptables -t nat -A POSTROUTING -o ppp0 -s 192.168.0.98 -j MASQUERADE
 
 }
 
 turn_self_normal() {
 	
 	echo "->turn_self_normal<-"
-	PPP_PID=$(pidof pppd)
-	if [ $PPP_PID ];
+	MYPPP_PID=$(pidof pppd)
+	if [ $MYPPP_PID ];
 	then
-		kill -s SIGTERM $PPP_PID
+		kill -s SIGTERM $MYPPP_PID
 	fi
-
-	ifconfig  $MY_GATEWAY_INTF down
+	ifconfig $MY_GATEWAY_INTF $MY_IP up
+	ifconfig  $MY_SECONDARY_INTF down
+	/usr/sbin/iptables -t nat -D POSTROUTING -o ppp0 -s 192.168.0.98 -j MASQUERADE
 }
 
 
@@ -158,14 +183,14 @@ poll_gpio() {
 }
 
 ping_check() {
-	#pull out mac addr, compare and set MAC to ATOM|ME|UNKNOWN	
+
 	PING_COUNT=0
 	TO_PING=$1
 	COUNT=0
 
 	while [[ $COUNT -ne 5 ]]
 	do	
-		arping -c1 $TO_PING -w2 -I $MY_GATEWAY_INTF
+		/usr/bin/arping -c1 $TO_PING -w2 -I $MY_GATEWAY_INTF
 		# returns 1 if sucess,
 		# returns 0 if fail
 		
--- rootfs/etc/itxscripts/gateway_controller.sh
+++ rootfs/etc/itxscripts/gateway_controller.sh
@@ -14,7 +14,7 @@ set -x
 #               if down                 
 #                       bind 192.168.0.1            
 #                       start atom_boot_timer script
-#                       send wol end of timer script
+#                       send /usr/bin/wol end of timer script
 #            
 #2) Power cut                                                      
 #       1) check if atom server is up, run halt on the server if up
@@ -23,7 +23,11 @@ set -x
 #
 #router will have secondary ip with  br-lan:1 
 #log levels are
-LOG_LEVEL='2' 
+export HOME=/root
+
+LOG_LEVEL='0' 
+LOG_FILE=/etc/itxscripts/gateway_controller.log
+> $LOG_FILE
 # 0 - error logs only
 # 1 - error + debug logs
 # 2 - error + debug  + info logs
@@ -32,8 +36,9 @@ WOL_SENT_COUNT=0;
 ATOM_MAC="00:1c:c0:d5:ed:60"
 
 MY_IP='192.168.0.4'
-MY_SECONDARY_IP='192.168.0.140'
-MY_GATEWAY_INTF='br-lan:1' #brwlan
+MY_SECONDARY_INTF='br-lan:1' #swaps ips with gateway
+MY_GATEWAY_INTF='br-lan' #brwlan
+
 
 ATOM_IP='192.168.0.110'
 ATOM_GATEWAY_INTF='eth0:1'
@@ -41,14 +46,18 @@ ATOM_GATEWAY_INTF='eth0:1'
 GATEWAY_IP='192.168.0.1'
 
 POWERCUT_FLAG_COUNT=0;
-POWERCUT_FLAG_MAXCOUNT=3; # $POWERCUT_FLAG_COUNT * LOOP_INTERVAL = number of seconds the script would wait to take action on the router..
+POWERCUT_FLAG_MAXCOUNT=3; #36 -default $POWERCUT_FLAG_COUNT * LOOP_INTERVAL = number of seconds the script would wait to take action on the router..
 			   # once the script hits maxcount, we run halt	
+
+POWERRESUME_FLAG_COUNT=0;
+POWERRESUME_FLAG_MAXCOUNT=3; #60 -default
 
 MY_STATUS='UNKOWN'
 ATOM_STATUS='UNKOWN'
 POWER_STATUS='UNKOWN'
 GATEWAY_STATUS='UNKOWN'
-. functions.sh
+. /etc/itxscripts/functions.sh
+cp  /etc/itxscripts/known_hosts /root/.ssh/
 
 while [[ 1 ]];
 do	
@@ -83,95 +92,111 @@ do
 	if [ "$POWER_STATUS" == "ON" ]; #GPIO indicates power is ON
 	then	
 		logline debug "poll_gpio returned POWER_STATUS -> $POWER_STATUS"
+		powerresume_flag_check
 		POWERCUT_FLAG_COUNT=0; # since power is present, resetting flag to 0	
 		if [ "$MY_STATUS" == "NORMAL" ]; # Router isnt the gateway and power is present we need to ensure atom is gateway
 		then
-			logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS "
+				logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS "
 		
-			if [ "$ATOM_STATUS" == "DOWN" ]; # ATOM_IP is down, server down
-			then
-			
-				logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
-				if [ "$GATEWAY_STATUS" == "DOWN" ]; # NO one is using the gateway IP
+				if [ "$ATOM_STATUS" == "DOWN" ]; # ATOM_IP is down, server down
 				then
-					logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
-					logline info "Running wol on $ATOM_IP"
-
-					wol -i 192.168.0.255 $ATOM_MAC
-				
-					WOL_SENT_COUNT=$(( $WOL_SENT_COUNT + 1 ))
-					logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, WOL_SENT_COUNT -> $WOL_SENT_COUNT"
-					if [[ $WOL_SENT_COUNT -eq 15 ]];
+			
+					logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
+					if [ "$GATEWAY_STATUS" == "DOWN" ]; # NO one is using the gateway IP
 					then
-						logline error "WOL_SENT_COUNT -> 15, something wrong with the ATOM Server??"
-					fi	
+						logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
+						if [[ $POWERRESUME_FLAG_COUNT -eq $POWERRESUME_FLAG_MAXCOUNT ]];
+						then
+							logline info "Running /usr/bin/wol on $ATOM_IP"
 
-				elif [ "$GATEWAY_STATUS" == "UP" ]; # unkown on gateway ip
-				then
-					logline error "ping_check returned GATEWAY_IP -> REACHABLE"
-					logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MYSTATUS, ATOM_STATUS -> $ATOM_STATUS - but GATEWAY_STATUS -> $GATEWAY_STATUS :S"
-					#do nothing, 
-					#continue in loop
-				fi
+							/usr/bin/wol -i 192.168.0.255 $ATOM_MAC
+							WOL_SENT_COUNT=$(( $WOL_SENT_COUNT + 1 ))
+							logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, WOL_SENT_COUNT -> $WOL_SENT_COUNT"
+							if [[ $WOL_SENT_COUNT -eq 15 ]];
+							then
+								logline error "WOL_SENT_COUNT -> 15, something wrong with the ATOM Server??"
+							fi
+						else
+							logline info "wol skipped, POWERRESUME_FLAG_COUNT - $POWERRESUME_FLAG_COUNT needs to hit $POWERRESUME_FLAG_MAXCOUNT"
+							turn_self_gateway
 
-			####end ATOM_STATUS DOWN
-		 	elif [ "$ATOM_STATUS" == "UP" ]; #ATOM_IP reachable, ensure atom is the gateway - 
-			then	
+					
+	 			   		fi ##	 if [[ $POWERRESUME_FLAG_COUNT -eq $POWERRESUME_FLAG_MAXCOUNT ]];
 
+					elif [ "$GATEWAY_STATUS" == "UP" ]; # unkown on gateway ip
+					then
+						logline error "ping_check returned GATEWAY_IP -> REACHABLE"
+						logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MYSTATUS, ATOM_STATUS -> $ATOM_STATUS - but GATEWAY_STATUS -> $GATEWAY_STATUS :S"
+						#do nothing, 
+						#continue in loop
+					fi
 
-		 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
-				if [ "$GATEWAY_STATUS" == "UP" ]; # 
-		        	then
-                                	logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
-				 	#wrong to take for granted atom would be the gateway, could do better here - figure mac etc..
-				 	
-					TEMP=$(ssh -i id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF" | awk -F ':' '/inet addr/{print $2}' | sed -e 's/  Bcast//g' )	
-				 	if [ "$TEMP" == "$GATEWAY_IP" ];
-				 	then
-				 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, ATOM is GATEWAY \0/"
-						checkrun_atom_ppp
-					 else 
-				 		logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, ATOM is NOT the GATEWAY :S"
-						#do nothing,
-						#continue looping
-				 	fi
+				####end ATOM_STATUS DOWN
+		 		elif [ "$ATOM_STATUS" == "UP" ]; #ATOM_IP reachable, ensure atom is the gateway - 
+				then	
+					
+					
+		 			logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
+					if [ "$GATEWAY_STATUS" == "UP" ]; # 
+		        		then
+                                		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS"
+						#Hit two bugs at this point 
+						#- piping|redirecting  dropbear ssh output just doesnt work in non-interactive env
+						#- dropbear scp doesnt seem to honour ssh options like scp -P2222 -oStrictHostKeyChecking=no   root@192.168.0.133:/tmp/test1234 .
+						#- hence doing the below :s
+						/usr/bin/ssh -i /etc/itxscripts/id_rsa root@${ATOM_IP} -p2222 "ifconfig $ATOM_GATEWAY_INTF >/tmp/intf"
+						/usr/bin/scp -P2222 -i /etc/itxscripts/id_rsa root@${ATOM_IP}:/tmp/intf /tmp/intf
+						TEMP=$(cat /tmp/intf | awk -F ':' '/inet addr/{print $2}' | sed -e 's/  Bcast//g' )	
+						if [ "$TEMP" == "$GATEWAY_IP" ];
+				 		then
+				 			logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, ATOM is GATEWAY \0/"
+							checkrun_atom_ppp
+						 else 
+				 			logline error "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS TEMP -> $TEMP, ATOM is NOT the GATEWAY :S"
+							#do nothing,
+							#continue looping
+				 		fi
 		        	
-				elif [ "$GATEWAY_STATUS" == "DOWN" ];
-			 	then
+					elif [ "$GATEWAY_STATUS" == "DOWN" ];
+			 		then
 			 		
-					logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, Turning atom into gateway .."
-					turn_atom_gateway
-				fi
+						logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS, GATEWAY_STATUS -> $GATEWAY_STATUS, Turning atom into gateway .."
+						turn_atom_gateway
+					fi
 			
-			fi #end ATOM_STATUS UP
-		elif [ "$MY_STATUS" == "GATEWAY" ]; #Power is present, router is the gateway - we need to check and turn atom into the gateway
-		then	
-			logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS"
-			if [ "$ATOM_STATUS" == "DOWN" ]; # ATOM_IP is down, 
-			then
-			
-				logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
-				logline info "Running wol on $ATOM_IP"
-
-
-				wol -i 192.168.0.255 $ATOM_MAC
-				WOL_SENT_COUNT=$(( $WOL_SENT_COUNT + 1 ))
-				logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, WOL_SENT_COUNT -> $WOL_SENT_COUNT"
-				if [[ $WOL_SENT_COUNT -eq 15 ]];
-				then
-					logline error "WOL_SENT_COUNT -> 15, something wrong with the ATOM Server??"
-				fi	
-
-			#end ATOM_STATUS DOWN
-		 	elif [ "$ATOM_STATUS" == "UP" ]; #ATOM_IP UP, ensure atom is the gateway - 
+				fi #end ATOM_STATUS UP
+			elif [ "$MY_STATUS" == "GATEWAY" ]; #Power is present, router is the gateway - we need to check and turn atom into the gateway
 			then	
-
-		 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
-				turn_self_normal
-				turn_atom_gateway
+				logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS"
+				if [ "$ATOM_STATUS" == "DOWN" ]; # ATOM_IP is down, 
+				then
 			
-			fi #end ATOM_IP reachable
-		fi	
+					logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
+					if [[ $POWERRESUME_FLAG_COUNT -eq $POWERRESUME_FLAG_MAXCOUNT ]];
+					then		
+						logline info "Running /usr/bin/wol on $ATOM_IP"
+
+						/usr/bin/wol -i 192.168.0.255 $ATOM_MAC
+						WOL_SENT_COUNT=$(( $WOL_SENT_COUNT + 1 ))
+						logline debug "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, WOL_SENT_COUNT -> $WOL_SENT_COUNT"
+						if [[ $WOL_SENT_COUNT -eq 15 ]];
+						then
+							logline error "WOL_SENT_COUNT -> 15, something wrong with the ATOM Server??"
+						fi
+					else
+						logline info "wol skipped, POWERRESUME_FLAG_COUNT - $POWERRESUME_FLAG_COUNT needs to hit $POWERRESUME_FLAG_MAXCOUNT"
+					
+	 			   	fi ##	 if [[ $POWERRESUME_FLAG_COUNT -eq $POWERRESUME_FLAG_MAXCOUNT ]];
+				#end ATOM_STATUS DOWN
+		 		elif [ "$ATOM_STATUS" == "UP" ]; #ATOM_IP UP, ensure atom is the gateway - 
+				then	
+
+		 				logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS"
+						turn_self_normal
+						turn_atom_gateway
+			
+				fi #end ATOM_IP reachable
+			fi	
 
 
 ################################POWER OFF###############################################
@@ -181,6 +206,7 @@ do
 		logline debug "poll_gpio returned POWER_STATUS -> $POWER_STATUS"
 
 		powercut_flag_check
+		POWERRESUME_FLAG_COUNT=0;
 		logline info "POWERCUT_FLAG_COUNT -> $POWERCUT_FLAG_COUNT"
 		if [[ $POWERCUT_FLAG_COUNT -eq $POWERCUT_FLAG_MAXCOUNT  ]]; #POWERCUT_FLAG_MAXCOUNT hit, we check atom, gateway - initiate turn_gateway 
 		then
@@ -213,15 +239,6 @@ do
 				
 					turn_atom_normal poweroff
 				 ####TBC#####   	   
-					ping_check $GATEWAY_IP
-			                if [ "$STATUS" == "DOWN" ];
-                			then
-                        			GATEWAY_STATUS=DOWN
-                			elif [ "$STATUS" == "UP" ];
-                			then
-                        			GATEWAY_STATUS=UP
-                			fi
-					
 					if [ "$GATEWAY_STATUS" == "UP" ]; # 
 		        		then
                                 		logline info "POWER_STATUS -> $POWER_STATUS, MY_STATUS -> $MY_STATUS, ATOM_STATUS -> $ATOM_STATUS,  GATEWAY_STATUS -> $GATEWAY_STATUS"
